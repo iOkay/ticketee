b6f37a144c4691ff0f27ba742c673e92cfe2a6c4
iOkay <daiyunxing@pwrr.com>
2016-02-03 17:01:51 +0800

    Add the ablilty to view a list of all projects

---
+++ spec/features/viewing_projects_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+require "factory_girl"
+
+RSpec.feature "User can view projects" do
+  scenario "with the project details" do
+    project = FactoryGirl.create(:project, name: "Sublime Text 3")
+
+    visit "/"
+
+    click_link "Sublime Text 3"
+    expect(page.current_url).to eq project_url(project)
+  end
+end
